065e0dc43cdde317f87d9a765c5c0afd774d3051
Steve Faletti <stevefaletti@gmail.com>
2017-10-01 22:54:41 -0400

    swap enum for a simpler integer holder.
    The enum can be used in a main loop to make states easier to understand and reason about.

--- lib/SaberStateMachine/SaberStateMachine.h
+++ lib/SaberStateMachine/SaberStateMachine.h
@@ -11,24 +11,14 @@ Proprietary to Richard Clarkson Studio
 class SaberStateMachine
 {
 public:
-  // Add any new lighting animations to the States enum. Make sure they are placed before STATES_ENUM_LENGTH.
-  enum States {
-    OFF,
-    FALLING_DOT,
-    MIDDLE_OUT,
-    RIPPLE,
-    FADE,
-    MUSIC_RAINBOW,
-    LAMP_MODE,
-    STATES_ENUM_LENGTH
-  };
-  SaberStateMachine();
+  SaberStateMachine(int statesCount);
   int getCurrent();
   void setCurrent(int newState);
   void next();
 
 private:
   int _currentState;
+  int _statesCount;
 };
 
 #endif
