e846c0c7ed52c87d7308144035fdef8f550b4937
Steve Faletti <stevefaletti@gmail.com>
2017-10-02 07:26:19 -0400

    test that button library will build
    Tests that the linker in PlatformIO works as expected. This code will likely be junked later. The test was successful.

--- src/main.cpp
+++ src/main.cpp
@@ -1,9 +1,14 @@
 #include <Arduino.h>
+#include <SaberButton.h>
+
+SaberButton capSensor(7);
+
+unsigned long pressTime = 0;
 
 void setup() {
     // put your setup code here, to run once:
 }
 
 void loop() {
-    // put your main code here, to run repeatedly:
+    pressTime = capSensor.getPressTime();
 }
