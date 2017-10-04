d92ec607ea3e2d9f6aafbb7867fef7acd6e6ef4e
Steve Faletti <stevefaletti@gmail.com>
2017-10-03 22:54:57 -0400

    add capabilities to sense taps & presses
    Taps are recorded when the button is released, presses are recorded while the button is held down.

--- lib/SaberButton/SaberButton.cpp
+++ lib/SaberButton/SaberButton.cpp
@@ -13,29 +13,70 @@ SaberButton::SaberButton(int pin)
 	_pin = pin;
 	_prevMillis = 0;
 	_pressMillis = 0;
+	_pressType = 0; // 0:no press, 1: tap, 2: press
+	tapThreshold = 50;
+	tapLength = 300;
+	pressThreshold = 500;
+	pressLength = 1000;
 }
 
 // This function should be called repeatedly in the main loop. It assumes a debounced button, though in actual application it will just wait until the button has settled before beginning to count, which won't be noticed by most users.
-long SaberButton::getPressTime()
+void SaberButton::check()
 {
 	if (digitalRead(_pin) == HIGH)
 	{
-		unsigned long currentMillis = millis();
-		// Check that some time has passed
-		if (currentMillis > _prevMillis)
+		_pressMillis += _getPressMillis();
+		if (_pressMillis > pressThreshold)
 		{
-			_pressMillis += currentMillis - _prevMillis;
-			_prevMillis = currentMillis;
+			_pressType = 2;
 		}
-		else
+	}
+	else // _pin is LOW
+	{
+		// too short for a tap (bouncing), or between a tap and press
+		if (_pressMillis <= tapThreshold || (_pressMillis > tapLength + tapThreshold && _pressMillis <= pressThreshold))
 		{
-			// in the unlikely case of a millis() overflow this will wait for the counter to restart. This will likely not be noticed in normal use.
+			_pressType = 0; // no press recorded
 		}
+		// tap recorded
+		else if (_pressMillis > tapThreshold && _pressMillis <= tapLength)
+		{
+			_pressType = 1; // tap press type recorded
+		}
+	}
+}
+
+char SaberButton::getPressType()
+{
+	char pressTypeHolder = _pressType;
+	_pressType = 0;
+	return pressTypeHolder;
+}
+
+unsigned long SaberButton::getPressLength() {
+	unsigned long pressMillisHolder = _pressMillis;
+	_pressMillis = 0;
+	return pressMillisHolder;	
+}
+
+void SaberButton::_clearPrevMillis()
+{
+	_prevMillis = 0;
+}
+
+unsigned long SaberButton::_getPressMillis()
+{
+	unsigned long currentMillis = millis();
+	unsigned long returnMillis = 0;
+	// Check that some time has passed
+	if (currentMillis > _prevMillis)
+	{
+		returnMillis = currentMillis - _prevMillis;
+		_prevMillis = currentMillis;
 	}
 	else
 	{
-		_pressMillis = 0;
-		_prevMillis = 0;
+		// in the unlikely case of a millis() overflow this will wait for the counter to restart. This will likely not be noticed in normal use.
 	}
-	return _pressMillis;
+	return returnMillis;
 }
