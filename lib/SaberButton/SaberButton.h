47ff3b528fbc48cb7b03b590ef541bde304d67ab
Steve Faletti <stevefaletti@gmail.com>
2017-10-03 22:54:57 -0400

    add capabilities to sense taps & presses
    Taps are recorded when the button is released, presses are recorded while the button is held down.

--- lib/SaberButton/SaberButton.h
+++ lib/SaberButton/SaberButton.h
@@ -11,12 +11,21 @@ class SaberButton
 {
 public:
 	SaberButton(int pin);
-	long getPressTime();
-
+	void check();
+	char getPressType();
+	unsigned long getPressLength();
+	unsigned long tapThreshold;
+	unsigned long tapLength;
+	unsigned long pressThreshold;
+	unsigned long pressLength;
+	
 private:
+	void _clearPrevMillis();
+	unsigned long _getPressMillis();
 	char _pin;
 	unsigned long _prevMillis;
 	unsigned long _pressMillis;
+	char _pressType;
 };
 
 #endif
