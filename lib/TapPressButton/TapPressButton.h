4716412f01d804a55ce8206a721f991c54052c84
Steve Faletti <stevefaletti@gmail.com>
2017-10-13 10:40:55 -0400

    check for press type and get count

--- lib/TapPressButton/TapPressButton.h
+++ lib/TapPressButton/TapPressButton.h
@@ -23,6 +23,7 @@ private:
   void updatePressTime(unsigned long newTime);
   bool isTap();
   bool isPress();
+  int pressCount;
   int getPressCount();
   void resetTimeStamp();
   void resetPressTime();
