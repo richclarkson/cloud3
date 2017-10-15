875805bd67c845ae6587cb4c755661de5fab0070
Steve Faletti <stevefaletti@gmail.com>
2017-10-15 11:40:44 -0400

    add 1st pass unit test for button class

--- test/test_button/test_button.cpp
+++ test/test_button/test_button.cpp
@@ -1,15 +1,35 @@
+#ifdef UNIT_TEST
+
 #include <TapPressButton.h>
 #include <unity.h>
 
-#ifndef UNIT_TEST
-
-void setup() {
-    //start the test
-    UNITY_BEGIN();
+void test_bounce(void) {
+  TapPressButton btn = TapPressButton();
+  for (int i=0; i<60; i++) {
+      btn.update(true, i);
+      TEST_ASSERT_EQUAL(btn.getPressType(), 0);
+  }
+  btn.update(false, 70);
+  TEST_ASSERT_EQUAL(btn.getPressType(), 1);
+  btn.update(true, 500);
+  TEST_ASSERT_EQUAL(btn.getPressType(), 0);
+  btn.update(true, 551);
+  TEST_ASSERT_EQUAL(btn.getPressType(), 0);
+  btn.update(false, 555);
+  TEST_ASSERT_EQUAL(btn.getPressType(), 1);
 }
 
-void loop() {
-    UNITY_END();
+void test_tap() {
+    TapPressButton btn = TapPressButton();
+    btn.update(true, 70);
+}
+
+int main(int argc, char **argv) {
+  UNITY_BEGIN();
+
+  RUN_TEST(test_bounce);
+
+  UNITY_END();
 }
 
 #endif
