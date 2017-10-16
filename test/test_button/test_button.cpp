283b2bcb1191c7500bee46db38c8e1122241ec34
Steve Faletti <stevefaletti@gmail.com>
2017-10-16 00:23:48 -0400

    muck with the button test

--- test/test_button/test_button.cpp
+++ test/test_button/test_button.cpp
@@ -3,24 +3,18 @@
 #include <TapPressButton.h>
 #include <unity.h>
 
-void test_tap() {
+void test_debounce() {
   TapPressButton btn = TapPressButton();
-  btn.update(false, 10);
+  btn.update(false, 0);
   btn.update(true, 30);
-  TEST_ASSERT_EQUAL(btn.getPressType(), 0);
-  btn.update(false, 100);
-  btn.update(true, 130);
-  TEST_ASSERT_EQUAL(btn.getPressType(), 0);
-  btn.update(true, 200);
-  TEST_ASSERT_EQUAL(btn.getPressType(), 0);
-  btn.update(false, 201);
-  TEST_ASSERT_EQUAL(btn.getPressType(), 1);
+  TEST_ASSERT_FALSE(btn.isTap());
+  // TEST_ASSERT_FALSE(btn.isPress());
 }
 
 int main(int argc, char **argv) {
   UNITY_BEGIN();
 
-  RUN_TEST(test_tap);
+  RUN_TEST(test_debounce);
 
   UNITY_END();
 }
