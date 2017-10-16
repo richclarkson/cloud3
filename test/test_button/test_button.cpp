6ade4b71204fbf448b402ba4fdcf71eb9182a623
Steve Faletti <stevefaletti@gmail.com>
2017-10-15 22:58:58 -0400

    add more kludgy tests

--- test/test_button/test_button.cpp
+++ test/test_button/test_button.cpp
@@ -3,31 +3,24 @@
 #include <TapPressButton.h>
 #include <unity.h>
 
-void test_bounce(void) {
-  TapPressButton btn = TapPressButton();
-  for (int i=0; i<60; i++) {
-      btn.update(true, i);
-      TEST_ASSERT_EQUAL(btn.getPressType(), 0);
-  }
-  btn.update(false, 70);
-  TEST_ASSERT_EQUAL(btn.getPressType(), 1);
-  btn.update(true, 500);
-  TEST_ASSERT_EQUAL(btn.getPressType(), 0);
-  btn.update(true, 551);
-  TEST_ASSERT_EQUAL(btn.getPressType(), 0);
-  btn.update(false, 555);
-  TEST_ASSERT_EQUAL(btn.getPressType(), 1);
-}
-
 void test_tap() {
-    TapPressButton btn = TapPressButton();
-    btn.update(true, 70);
+  TapPressButton btn = TapPressButton();
+  btn.update(false, 10);
+  btn.update(true, 30);
+  TEST_ASSERT_EQUAL(btn.getPressType(), 0);
+  btn.update(false, 100);
+  btn.update(true, 130);
+  TEST_ASSERT_EQUAL(btn.getPressType(), 0);
+  btn.update(true, 200);
+  TEST_ASSERT_EQUAL(btn.getPressType(), 0);
+  btn.update(false, 201);
+  TEST_ASSERT_EQUAL(btn.getPressType(), 1);
 }
 
 int main(int argc, char **argv) {
   UNITY_BEGIN();
 
-  RUN_TEST(test_bounce);
+  RUN_TEST(test_tap);
 
   UNITY_END();
 }
