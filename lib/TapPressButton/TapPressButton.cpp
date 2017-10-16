ee7bb821aa12c8614c00a8ebd47a168afc412695
Steve Faletti <stevefaletti@gmail.com>
2017-10-15 23:15:22 -0400

    simplify press/tap identification
    The integer representing press or tap types was weird and confusion. This pulls the checks for presses and taps into the public declaration. It also starts getting rid of some cruft that was semi-related.

--- lib/TapPressButton/TapPressButton.cpp
+++ lib/TapPressButton/TapPressButton.cpp
@@ -4,7 +4,7 @@ TapPressButton::TapPressButton() {
   prevTimeStamp = 0;
   pressTime = 0;
   pressCount = 0;
-  pressType = 0;
+  buttonState = false;
 
   // set default values for button behavior
   setDebounce(50);
@@ -15,23 +15,25 @@ TapPressButton::TapPressButton() {
 
 void TapPressButton::update(bool btnVal, unsigned long timerVal) {
   // This function should be called repeatedly in the main loop.
-  pressType = 0;
-  if (btnVal) {
+  buttonState = btnVal;
+  if (buttonState) {
     updatePressTime(timerVal);
-    if (isPress()) {
-      pressType = 2;
-      pressCount = getPressCount();
-    }
   } else {
-    if (isTap()) {
-      pressType = 1;
-    }
     resetPressTime();
   }
 }
 
 bool TapPressButton::isDebounced() {
   return pressTime > DEBOUNCE_THRESHOLD;
+}
+
+bool TapPressButton::isTap() {
+  return !buttonState && pressTime > DEBOUNCE_THRESHOLD &&
+         pressTime <= DEBOUNCE_THRESHOLD + TAP_LENGTH;
+}
+
+bool TapPressButton::isPress() {
+  return buttonState && pressTime > PRESS_THRESHOLD;
 }
 
 void TapPressButton::updatePressTime(unsigned long newTime) {
@@ -41,12 +43,7 @@ void TapPressButton::updatePressTime(unsigned long newTime) {
   }
 }
 
-bool TapPressButton::isTap() {
-  return pressTime > DEBOUNCE_THRESHOLD &&
-         pressTime <= DEBOUNCE_THRESHOLD + TAP_LENGTH;
-}
-
-bool TapPressButton::isPress() { return pressTime > PRESS_THRESHOLD; }
+void TapPressButton::resetPressTime() { pressTime = 0; }
 
 int TapPressButton::getPressCount() {
   int pressCount = 0;
@@ -67,12 +64,8 @@ int TapPressButton::getPressCount() {
   return pressCount;
 }
 
-void TapPressButton::resetTimeStamp() { prevTimeStamp = 0; }
+char TapPressButton::getPressType() { return pressType; }
 
-void TapPressButton::resetPressTime() { pressTime = 0; }
-
-char TapPressButton::getPressType() {
-  return pressType;
 void TapPressButton::setDebounce(unsigned long debounceTime) {
   DEBOUNCE_THRESHOLD = debounceTime;
 }
