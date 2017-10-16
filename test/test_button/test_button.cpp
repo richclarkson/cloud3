535841a4fb0512008a8b23809f0a481dbe2ddfe3
Steve Faletti <stevefaletti@gmail.com>
2017-10-16 00:41:32 -0400

    shorten debounce test declaration

--- test/test_button/test_button.cpp
+++ test/test_button/test_button.cpp
@@ -4,11 +4,18 @@
 #include <unity.h>
 
 void test_debounce() {
+  int testTimes[8] = {0,20,50,90,1000,1049,50000,50030};
+  int testTimesLen = 8;
+  bool btnSwitch = true;
   TapPressButton btn = TapPressButton();
-  btn.update(false, 0);
-  btn.update(true, 30);
-  TEST_ASSERT_FALSE(btn.isTap());
-  // TEST_ASSERT_FALSE(btn.isPress());
+  for (int i=testTimes[0]; i<testTimesLen; i++) {
+    btn.update(btnSwitch, testTimes[i]);
+    btn.update(!btnSwitch, testTimes[i]);
+    TEST_ASSERT_FALSE(btn.isTap());
+    TEST_ASSERT_FALSE(btn.isPress());
+  }
+}
+
 }
 
 int main(int argc, char **argv) {
