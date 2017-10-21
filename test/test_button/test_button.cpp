99e4671b573efe97fa31fcf51435d390994f95a0
Steve Faletti <stevefaletti@gmail.com>
2017-10-21 16:06:24 -0400

    add tests to check press count
    these all passed

--- test/test_button/test_button.cpp
+++ test/test_button/test_button.cpp
@@ -88,12 +88,40 @@ void Test_Presses() {
   test_press(893459837);
 }
 
+void test_press_count(unsigned long start, unsigned long interval,
+                      int pressCount) {
+  TapPressButton btn = TapPressButton();
+  unsigned long timerVal = start;
+  unsigned long timerEnd = start + interval;
+  while (timerVal < timerEnd) {
+    btn.update(true, timerVal);
+    timerVal++;
+  }
+  TEST_ASSERT_TRUE(btn.isPress());
+  TEST_ASSERT_EQUAL_INT(pressCount, btn.getPressCount());
+}
+
+void Test_Press_Counts() {
+  test_press_count(0, 501, 1);
+  test_press_count(0, 1000, 1);
+  test_press_count(0, 1500, 1);
+  test_press_count(0, 1501, 2);
+  test_press_count(0, 2000, 2);
+  test_press_count(0, 2500, 2);
+  test_press_count(0, 3000, 3);
+  test_press_count(100, 4000, 4);
+  test_press_count(1000000, 5000, 5);
+  test_press_count(4294967295ul, 6000, 6);
+  test_press_count(4294967295ul, 7000, 7);
+}
+
 int main(int argc, char **argv) {
   UNITY_BEGIN();
 
   RUN_TEST(Test_Debounces);
   RUN_TEST(Test_Taps);
   RUN_TEST(Test_Presses);
+  RUN_TEST(Test_Press_Counts);
 
   UNITY_END();
 }
