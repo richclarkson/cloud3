cb19696d85c669a397eb4083879e11c578566aa7
Steve Faletti <stevefaletti@gmail.com>
2017-10-21 16:32:47 -0400

    add some physical test code

--- src/main.cpp
+++ src/main.cpp
@@ -6,21 +6,25 @@
 TapPressButton capSensor = TapPressButton();
 
 const int btnPin = 7;
-
+bool LEDVal = false;
 
 void setup() {
     pinMode(btnPin, INPUT);
+    pinMode(LED_BUILTIN, OUTPUT);
 }
 
 void loop() {
     capSensor.update(digitalRead(btnPin), millis());
     if (capSensor.isTap()) {
+        LEDVal = !LEDVal;
         // do tap things
     }
     else if (capSensor.isPress()) {
-        int pressCount = capSensor.getPressCount();
+        // int pressCount = capSensor.getPressCount();
         // do press things, likely with pressCount
     }
+
+    digitalWrite(LED_BUILTIN, LEDVal);
 }
 
 #endif
