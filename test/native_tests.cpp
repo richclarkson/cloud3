21af1124f418bdfdc95b57775c9e6286bc4ae726
Steve Faletti <stevefaletti@gmail.com>
2017-10-25 08:57:35 -0700

    add TapPressButton existence

--- test/native_tests.cpp
+++ test/native_tests.cpp
@@ -2,8 +2,21 @@
 
 #include <unity.h>
 
+class TapPressButton {
+public:
+    void updateInput(bool _input) {
+        input = _input;
+    }
+};
+
+void testTapPressButtonExists() {
+    TapPressButton btn;
+}
+
 int main(int argc, char **argv) {
     UNITY_BEGIN();
+
+    RUN_TEST(testTapPressButtonExists);
 
     UNITY_END();
 }
