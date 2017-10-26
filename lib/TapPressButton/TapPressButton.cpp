bd9c056e96e12d7889b7e61a69ec19536f76b104
Steve Faletti <stevefaletti@gmail.com>
2017-10-25 23:16:04 -0700

    autoformat code

--- lib/TapPressButton/TapPressButton.cpp
+++ lib/TapPressButton/TapPressButton.cpp
@@ -1,19 +1,15 @@
 #include "TapPressButton.h"
 
-    void TapPressButton::updateInput(bool input, unsigned long timerVal) {
-        prevButtonState = currentButtonState;
-        currentButtonState = input;
-        pressTime += timerVal;
-    }
-    
-    bool TapPressButton::getCurrentState() {
-        return currentButtonState;
-    }
-    
-    bool TapPressButton::hasStateChanged() {
-        return currentButtonState != prevButtonState;
-    }
+void TapPressButton::updateInput(bool input, unsigned long timerVal) {
+  prevButtonState = currentButtonState;
+  currentButtonState = input;
+  pressTime += timerVal;
+}
 
-    unsigned long TapPressButton::getPressTime() {
-      return pressTime;
-    }
+bool TapPressButton::getCurrentState() { return currentButtonState; }
+
+bool TapPressButton::hasStateChanged() {
+  return currentButtonState != prevButtonState;
+}
+
+unsigned long TapPressButton::getPressTime() { return pressTime; }
