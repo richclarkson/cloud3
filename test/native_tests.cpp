593d63a5c340525b91c2abd3a4e15fc2d3e1cfc6
Steve Faletti <stevefaletti@gmail.com>
2017-10-27 11:11:42 -0700

    autoformat

--- test/native_tests.cpp
+++ test/native_tests.cpp
@@ -16,28 +16,29 @@ void testPressInPressWindow() {
   TEST_ASSERT_TRUE(btn.isPressInPressWindow());
 }
 
-void test_TapAfterInterval(TapPressButton &btn, unsigned long startTime, int interval, bool testType) {
-    unsigned long timerVal = startTime;
-    unsigned long endTime = startTime + interval;
-    while (timerVal < endTime) {
-        btn.updateInput(true, timerVal);
-        TEST_ASSERT_FALSE(btn.isTap());
-        timerVal ++;
-    }
-    btn.updateInput(false, timerVal);
-    if (testType == true) {
-        TEST_ASSERT_TRUE(btn.isTap());
-    } else {
-        TEST_ASSERT_FALSE(btn.isTap());
-    }
+void test_TapAfterInterval(TapPressButton &btn, unsigned long startTime,
+                           int interval, bool testType) {
+  unsigned long timerVal = startTime;
+  unsigned long endTime = startTime + interval;
+  while (timerVal < endTime) {
+    btn.updateInput(true, timerVal);
+    TEST_ASSERT_FALSE(btn.isTap());
+    timerVal++;
+  }
+  btn.updateInput(false, timerVal);
+  if (testType == true) {
+    TEST_ASSERT_TRUE(btn.isTap());
+  } else {
+    TEST_ASSERT_FALSE(btn.isTap());
+  }
 }
 
 void testMultipleDebounces() {
-    TapPressButton btn;
-    test_TapAfterInterval(btn, 0, 30, false);
-    test_TapAfterInterval(btn, 0, 20, false);
-    test_TapAfterInterval(btn, 5000, 40, false);
-    test_TapAfterInterval(btn, 87465439876, 30, false);
+  TapPressButton btn;
+  test_TapAfterInterval(btn, 0, 30, false);
+  test_TapAfterInterval(btn, 0, 20, false);
+  test_TapAfterInterval(btn, 5000, 40, false);
+  test_TapAfterInterval(btn, 87465439876, 30, false);
 }
 
 void testMultipleTaps() {
