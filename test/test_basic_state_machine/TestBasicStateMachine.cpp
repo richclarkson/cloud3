e2983d8ab7ef0cc06d06af615639dc96a8ac4531
Steve Faletti <stevefaletti@gmail.com>
2017-11-12 09:40:21 -0500

    rename test

--- test/test_basic_state_machine/TestBasicStateMachine.cpp
+++ test/test_basic_state_machine/TestBasicStateMachine.cpp
@@ -12,14 +12,14 @@ void tearDown(void) {
 
 }
 
-void test_state_machine_should_return_state(void) {
+void test_state_machine_should_return_initial_state(void) {
     TEST_ASSERT_EQUAL(0, stateMachine.getState());
 }
 
 int main(void) {
   UNITY_BEGIN();
 
-  RUN_TEST(test_state_machine_should_return_state);
+  RUN_TEST(test_state_machine_should_return_initial_state);
 
   UNITY_END();
 }
