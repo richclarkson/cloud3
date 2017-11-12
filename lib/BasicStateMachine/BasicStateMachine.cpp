1cdea006e7841799d9c23fbdd4fe79c578f18ca9
Steve Faletti <stevefaletti@gmail.com>
2017-11-12 09:26:49 -0500

    strip out cruft from old state machine

--- lib/BasicStateMachine/BasicStateMachine.cpp
+++ lib/BasicStateMachine/BasicStateMachine.cpp
@@ -1,8 +1,5 @@
-/* 
-Basic State Machine
-A very simple finite state machine library
-Created by Steve Faletti, 2017-10-01
-Proprietary to Richard Clarkson Studio
-*/
-
 #include "BasicStateMachine.h"
+
+BasicStateMachine::BasicStateMachine() {
+    int thing = 0;
+}
--- lib/BasicStateMachine/BasicStateMachine.h
+++ lib/BasicStateMachine/BasicStateMachine.h
@@ -1,16 +1,10 @@
-/* 
-Basic State Machine
-A very simple finite state machine library
-Created by Steve Faletti, 2017-10-01
-Proprietary to Richard Clarkson Studio
-*/
-
 #ifndef BasicStateMachine_h
 #define BasicStateMachine_h
 
-class BasicStateMachine
-{
+class BasicStateMachine {
 public:
+  BasicStateMachine();
+
 private:
 };
 
