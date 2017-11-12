ccacdc895036e399b0af94e5b6e9d49593b95b41
Steve Faletti <stevefaletti@gmail.com>
2017-11-12 07:43:02 -0500

    rename state machine library

---
+++ lib/BasicStateMachine/BasicStateMachine.cpp
@@ -0,0 +1,8 @@
+/* 
+Basic State Machine
+A very simple finite state machine library
+Created by Steve Faletti, 2017-10-01
+Proprietary to Richard Clarkson Studio
+*/
+
+#include "BasicStateMachine.h"
---
+++ lib/BasicStateMachine/BasicStateMachine.h
@@ -0,0 +1,17 @@
+/* 
+Basic State Machine
+A very simple finite state machine library
+Created by Steve Faletti, 2017-10-01
+Proprietary to Richard Clarkson Studio
+*/
+
+#ifndef BasicStateMachine_h
+#define BasicStateMachine_h
+
+class BasicStateMachine
+{
+public:
+private:
+};
+
+#endif
