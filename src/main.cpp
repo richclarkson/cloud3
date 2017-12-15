2690650cb15bf0bc3863f5fe73c548da6019525d
Steve Faletti <stevefaletti@gmail.com>
2017-12-14 23:03:37 -0500

    remove old state machine code

--- src/main.cpp
+++ src/main.cpp
@@ -2,14 +2,8 @@
 
 #include <Arduino.h>
 #include <TapPressButton.h>
-#include <BasicStateMachine.h>
-#include <SaberStateHandler.h>
 
 TapPressButton capSensor;
-
-BasicStateMachine mainState;
-BasicStateMachine settingState;
-BasicStateMachine lampState;
 
 const int capPin = 23;
 const int touchTime = 50;
@@ -17,10 +11,6 @@ unsigned long loopTime;
 bool isTouch;
 
 void setup() {
-    mainState = BasicStateMachine(7);
-    settingState = BasicStateMachine(5);
-    lampState = BasicStateMachine(4);
-
     loopTime = 0;
 
     capSensor = TapPressButton();
