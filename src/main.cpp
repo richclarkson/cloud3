aa6059a19af8979d1f52d4eed862082fd89898c7
Steve Faletti <stevefaletti@gmail.com>
2017-12-23 22:03:19 -0500

    run basic integration test in main file

--- src/main.cpp
+++ src/main.cpp
@@ -1,35 +1,36 @@
 #ifndef UNIT_TEST
 
 #include <Arduino.h>
-#include <TapPressButton.h>
 #include <StateManager.h>
+#include <TapPressButton.h>
 
 TapPressButton capSensor;
 StateManager mainState;
 
 const int capPin = 23;
-const int touchTime = 50;
+const int touchTime = 1000;
 unsigned long loopTime;
 bool isTouch;
 
 void setup() {
-    loopTime = 0;
-    capSensor = TapPressButton();
-    mainState = StateManager();
-    // Serial.begin(9600);
-    isTouch = false;
+  Serial.begin(9600);
+  loopTime = 0;
+  capSensor = TapPressButton(50, 300, 1000, 1000);
+  mainState = StateManager();
+  isTouch = false;
 }
 
 void loop() {
-    isTouch = touchRead(capPin) > touchTime;
-    loopTime = millis();
-    capSensor.update(isTouch, loopTime);
-    if (capSensor.isTap()) {
-        mainState.tap();
-    }
-    if (capSensor.isPress()) {
-        mainState.press();
-    }
+  isTouch = touchRead(capPin) > touchTime;
+  loopTime = millis();
+  // Serial.println(touchRead(capPin));
+  capSensor.update(isTouch, loopTime);
+  if (capSensor.isTap()) {
+    mainState.tap();
+  }
+  if (capSensor.isPress()) {
+    mainState.press();
+  }
 }
 
 #endif
