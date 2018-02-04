56e1e4239ae4b055d66c7a2fa6abcd132f3bb8ff
Steve Faletti <stevefaletti@gmail.com>
2018-02-04 16:46:04 -0500

    remove debug work

--- test/test_state_manager/TestStateManager.cpp
+++ test/test_state_manager/TestStateManager.cpp
@@ -21,10 +21,7 @@ TestStateManager *sm;
 TestState *ts = new TestState1();
 
 void setUp(void) {
-  cout << "\n===setup====";
   sm = new TestStateManager(new TestState1);
-  sm->tap();
-  cout << "====end setup====\n";
 }
 
 void test_test_state_id_exists() { TEST_ASSERT_EQUAL(1, ts->getID()); }
@@ -33,8 +30,6 @@ void test_starting_state_val_is_1() {
   TEST_ASSERT_EQUAL(1, sm->getCurrentID());
 }
 
-  cout << "\n===================\n";
-  cout << "initial: " << sm << "\n\n";
 void test_tap_moves_state_from_one_to_two() {
   TEST_ASSERT_EQUAL(1, sm->getCurrentID());
   sm->tap();
@@ -45,8 +40,6 @@ void test_tap_moves_state_from_two_to_one() {
   sm->tap();
   TEST_ASSERT_EQUAL(2, sm->getCurrentID());
   sm->tap();
-  cout << "test done\n";
-  cout << "===================\n\n";
   TEST_ASSERT_EQUAL(1, sm->getCurrentID());
 }
 
