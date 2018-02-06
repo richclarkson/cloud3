902b7e12390e5a9b7a8fd46617fb3b583af13d5d
Steve Faletti <stevefaletti@gmail.com>
2018-02-06 18:01:56 -0500

    remove old tests

--- test/test_state_manager/TestStateManager.cpp
+++ test/test_state_manager/TestStateManager.cpp
@@ -11,50 +11,11 @@ StateManager *sm;
 TestState *ts = new TestStateOne();
 
 void setUp(void) {
-  sm = new TestStateManager(new TestStateOne);
-}
-
-void test_test_state_id_exists() { TEST_ASSERT_EQUAL(1, ts->getID()); }
-
-void test_starting_state_val_is_1() {
-  TEST_ASSERT_EQUAL(1, sm->getCurrentID());
-}
-
-void test_tap_moves_state_from_one_to_two() {
-  TEST_ASSERT_EQUAL(1, sm->getCurrentID());
-  sm->tap();
-  TEST_ASSERT_EQUAL(2, sm->getCurrentID());
-}
-
-void test_tap_moves_state_from_two_to_one() {
-  sm->tap();
-  TEST_ASSERT_EQUAL(2, sm->getCurrentID());
-  sm->tap();
-  TEST_ASSERT_EQUAL(1, sm->getCurrentID());
-}
-
-void test_press_moves_two_to_three() {
-  sm->tap(); //go to Two
-  sm->press();
-  TEST_ASSERT_EQUAL(3, sm->getCurrentID());
-}
-
-void test_press_moves_three_to_one() {
-  sm->tap();
-  sm->press();
-  sm->press();
-  TEST_ASSERT_EQUAL(1, sm->getCurrentID());
+  sm = new StateManager(new TestStateOne);
 }
 
 int main() {
   UNITY_BEGIN();
-
-  RUN_TEST(test_test_state_id_exists);
-  RUN_TEST(test_starting_state_val_is_1);
-  RUN_TEST(test_tap_moves_state_from_one_to_two);
-  RUN_TEST(test_tap_moves_state_from_two_to_one);
-  RUN_TEST(test_press_moves_two_to_three);
-  RUN_TEST(test_press_moves_three_to_one);
 
   UNITY_END();
 }
