3f57ab0e65ef2033d65015aee8b95ca7a4ec314a
Steve Faletti <stevefaletti@gmail.com>
2018-02-07 22:56:34 -0500

    add test coverage

--- test/test_state_manager/TestStateManager.cpp
+++ test/test_state_manager/TestStateManager.cpp
@@ -18,10 +18,29 @@ void test_test_state_has_id() {
   TEST_ASSERT_EQUAL(1, ts->getID());
 }
 
-void test_test_one_tap_moves_to_test_two_when_tapped() {
+void test_one_moves_to_two_when_tapped() {
   TEST_ASSERT_EQUAL(1, ts->getID());
   sm->tap();
   TEST_ASSERT_EQUAL(2, sm->getCurrentID());
+}
+
+void test_two_moves_to_one_when_tapped() {
+  sm->tap();
+  sm->tap();
+  TEST_ASSERT_EQUAL(1, sm->getCurrentID());
+}
+
+void test_two_moves_to_three_when_pressed() {
+  sm->tap();
+  sm->press();
+  TEST_ASSERT_EQUAL(3, sm->getCurrentID());
+}
+
+void test_three_moves_to_one_when_pressed() {
+  sm->tap();
+  sm->press();
+  sm->press();
+  TEST_ASSERT_EQUAL(1, sm->getCurrentID());
 }
 
 void test_color_is_changed_when_state_three_is_tapped() {
@@ -41,7 +60,10 @@ int main() {
   UNITY_BEGIN();
 
   RUN_TEST(test_test_state_has_id);
-  RUN_TEST(test_test_one_tap_moves_to_test_two_when_tapped);
+  RUN_TEST(test_one_moves_to_two_when_tapped);
+  RUN_TEST(test_two_moves_to_one_when_tapped);
+  RUN_TEST(test_two_moves_to_three_when_pressed);
+  RUN_TEST(test_three_moves_to_one_when_pressed);
   RUN_TEST(test_color_is_changed_when_state_three_is_tapped);
 
   UNITY_END();
