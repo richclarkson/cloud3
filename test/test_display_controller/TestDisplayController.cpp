bebb48b97ea3d6be8d257d68d353fa6a45051338
Steve Faletti <stevefaletti@gmail.com>
2018-02-10 16:15:12 -0500

    add test for state update methods

--- test/test_display_controller/TestDisplayController.cpp
+++ test/test_display_controller/TestDisplayController.cpp
@@ -12,6 +12,10 @@ public:
   int getTestVal() { return testVal; }
   void turnOff() { testVal = 10; }
   void displayFallingDot() { testVal = 11; }
+  void displayMiddleOut() { testVal = 12; }
+  void displayRipple() { testVal = 13; }
+  void displayBandAndFade() { testVal = 14; }
+  void displayRainbow() { testVal = 15; }
 };
 
 StateManager sm;
@@ -32,10 +36,34 @@ void test_display_can_be_called_from_state_when_manager_updated() {
   TEST_ASSERT_EQUAL(11, tdc.getTestVal());
 }
 
+void test_states_call_displays_as_expected() {
+  sm.update();
+  TEST_ASSERT_EQUAL(10, tdc.getTestVal());
+  sm.tap();
+  sm.update();
+  TEST_ASSERT_EQUAL(11, tdc.getTestVal());
+  sm.tap();
+  sm.update();
+  TEST_ASSERT_EQUAL(12, tdc.getTestVal());
+  sm.tap();
+  sm.update();
+  TEST_ASSERT_EQUAL(13, tdc.getTestVal());
+  sm.tap();
+  sm.update();
+  TEST_ASSERT_EQUAL(14, tdc.getTestVal());
+  sm.tap();
+  sm.update();
+  TEST_ASSERT_EQUAL(15, tdc.getTestVal());
+  sm.tap();
+  sm.update();
+  TEST_ASSERT_EQUAL(10, tdc.getTestVal());
+}
+
 int main() {
   UNITY_BEGIN();
 
   RUN_TEST(test_display_can_be_called_from_state_when_manager_updated);
+  RUN_TEST(test_states_call_displays_as_expected);
 
   UNITY_END();
 }
