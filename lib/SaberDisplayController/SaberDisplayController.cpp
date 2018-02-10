57652523c2c21c03e6c1416c79d4ad0717f00bc9
Steve Faletti <stevefaletti@gmail.com>
2018-02-10 17:08:10 -0500

    add display methods to display controller

--- lib/SaberDisplayController/SaberDisplayController.cpp
+++ lib/SaberDisplayController/SaberDisplayController.cpp
@@ -4,3 +4,27 @@
 void SaberDisplayController::turnOff() {
   // include the code to turn off the LEDs here
 }
+
+void SaberDisplayController::displayFallingDot() {
+  // add state display code here
+}
+
+void SaberDisplayController::displayMiddleOut() {
+  // add state display code here
+}
+
+void SaberDisplayController::displayRipple() {
+  // add state display code here
+}
+
+void SaberDisplayController::displayBangAndFade() {
+  // add state display code here
+}
+
+void SaberDisplayController::displayRainbow() {
+  // add state display code here
+}
+
+void SaberDisplayController::displayColorPreview() {
+  // add state display code here
+}
