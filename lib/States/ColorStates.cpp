3c647056e2e655d3149883f13cc10ebf6556702b
Steve Faletti <stevefaletti@gmail.com>
2018-02-10 20:56:33 -0500

    delete color state objects before creating new ones

--- lib/States/ColorStates.cpp
+++ lib/States/ColorStates.cpp
@@ -6,21 +6,21 @@ Ombre::Ombre() {}
 Fire::Fire() {}
 
 void Neon::press(StateManager *sm) {
-  sm->setColor(new White());
   delete this;
+  sm->setColor(new White());
 }
 
 void White::press(StateManager *sm) {
-  sm->setColor(new Ombre());
   delete this;
+  sm->setColor(new Ombre());
 }
 
 void Ombre::press(StateManager *sm) {
-  sm->setColor(new Fire());
   delete this;
+  sm->setColor(new Fire());
 }
 
 void Fire::press(StateManager *sm) {
-  sm->setColor(new White());
   delete this;
+  sm->setColor(new White());
 }
