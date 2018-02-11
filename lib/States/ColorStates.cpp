9e17d5ac8a72f0ccc18610eea39cb725dd1df79a
Steve Faletti <stevefaletti@gmail.com>
2018-02-10 19:11:12 -0500

    move comparable things near on another

--- lib/States/ColorStates.cpp
+++ lib/States/ColorStates.cpp
@@ -1,27 +1,24 @@
 #include "ColorStates.h"
 
 Neon::Neon() {}
+White::White() {}
+Ombre::Ombre() {}
+Fire::Fire() {}
 
 void Neon::press(StateManager *sm) {
   sm->setColor(new White());
   delete this;
 }
 
-White::White() {}
-
 void White::press(StateManager *sm) {
   sm->setColor(new Ombre());
   delete this;
 }
 
-Ombre::Ombre() {}
-
 void Ombre::press(StateManager *sm) {
   sm->setColor(new Fire());
   delete this;
 }
-
-Fire::Fire() {}
 
 void Fire::press(StateManager *sm) {
   sm->setColor(new White());
